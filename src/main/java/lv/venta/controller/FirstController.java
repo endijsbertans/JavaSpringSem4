cac53c2673603204836ebe4ca6cd7ac3836f085a
Your Name <you@example.com>
2024-04-04 12:06:20 +0300

    basic page with thymyleaf injection

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -1,8 +1,12 @@
 package lv.venta.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import lv.venta.Product;
 
 
 
@@ -19,5 +23,26 @@ public class FirstController {
 		model.addAttribute("mymsg", "Cavinaa pasaulit <3");
 		return "msg-page";
 	}
+	@GetMapping("/product/test")
+	public String getProductTest(Model model) {
+		Product myProduct = new Product("Zemene", "garda", 0.99f, 5);
+		model.addAttribute("zemene", myProduct);
+		return "product-page";
+	}
 	
+	@GetMapping("/products/")
+	public String getProduct(Model model) {
+		ArrayList<Product> allProducts = new ArrayList<Product>();
+		Product myProduct = new Product("Zemene", "garda", 0.99f, 5);
+		allProducts.add(myProduct);
+		
+		Product myProduct2 = new Product("Abols", "Sarkans", 1.99f, 3);
+		allProducts.add(myProduct2);
+		
+		Product myProduct3 = new Product("Kakis", "melns", 5.99f, 1);
+		allProducts.add(myProduct3);
+		
+		model.addAttribute("products", allProducts);
+		return "product-page-3";
+	}
 }
