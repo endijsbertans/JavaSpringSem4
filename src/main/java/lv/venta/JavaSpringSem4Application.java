20f6206e8fadd36e6bb47b17fe1cbee27f050e60
Your Name <you@example.com>
2024-04-25 12:10:15 +0300

    h2

--- src/main/java/lv/venta/JavaSpringSem4Application.java
+++ src/main/java/lv/venta/JavaSpringSem4Application.java
@@ -1,7 +1,14 @@
 package lv.venta;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.venta.repo.IProductRepo;
 
 @SpringBootApplication
 public class JavaSpringSem4Application {
@@ -9,5 +16,15 @@ public class JavaSpringSem4Application {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringSem4Application.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner testDatabaseLayer(IProductRepo productRepo) {
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				ArrayList<Product> allProducts = new ArrayList<>(Arrays.asList(new Product("Abols", "Sarkans", 0.99f, 5),new Product("Zemene", "Salda", 1.23f, 3),new Product("Arbuzs", "Roza", 3.99f, 2)));
+				productRepo.saveAll(allProducts);
+			}
+		};
+	}
 }
--- src/main/java/lv/venta/repo/IProductRepo.java
+++ src/main/java/lv/venta/repo/IProductRepo.java
@@ -2,6 +2,8 @@ package lv.venta.repo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import lv.venta.Product;
+
 public interface IProductRepo extends CrudRepository<Product, Integer>{
 
 }
