62338cdf03828e457d181ec69057b6b8fb8a6967
Your Name <you@example.com>
2024-04-25 11:51:22 +0300

    added repo

--- src/main/java/lv/venta/Product.java
+++ src/main/java/lv/venta/Product.java
@@ -1,5 +1,11 @@
 package lv.venta;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -16,8 +22,12 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 //@AllArgsConstructor
+@Table(name = "ProductTable")
+@Entity
 public class Product {
-	
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO )
 	private int id;
 	
 	private static int counter = 0;
@@ -25,19 +35,23 @@ public class Product {
 	@NotNull
 	@Pattern(regexp = "[A-Z]{1}[a-z ]+")
 	@Size(min = 3, max = 20)
+	@Column(name = "Title")
 	private String title;
 	
 	@NotNull
 	@Pattern(regexp = "[A-Za-z]+")
 	@Size(min = 4, max = 200)
+	@Column(name = "Description")
 	private String description;
 	
 	@Min(0)
 	@Max(10000)
+	@Column(name = "Price")
 	private float price;
 	
 	@Min(0)
 	@Max(100)
+	@Column(name = "Quantity")
 	private int quantity;
 	
 	public void setId() {
---
+++ src/main/java/lv/venta/repo/IProductRepo.java
@@ -0,0 +1,7 @@
+package lv.venta.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface IProductRepo extends CrudRepository<Product, Integer>{
+
+}
